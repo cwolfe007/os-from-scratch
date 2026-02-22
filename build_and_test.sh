fc091a9cb308a784d3b4f8061f8ab32bdc042aa4
Caleb Wolfe <cwolfe651@gmai.com>
2026-02-22 00:19:33 -0800

    remove no grapic to see vga terminal

--- build_and_test.sh
+++ build_and_test.sh
@@ -2,12 +2,12 @@
 set -e
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <file.asm>"
-    exit 1
+  echo "Usage: $0 <file.asm>"
+  exit 1
 fi
 
 ASM_FILE="$1"
 BIN_FILE="${ASM_FILE%.*}.bin"
 
 nasm "$ASM_FILE" -f bin -o "$BIN_FILE"
-qemu-system-x86_64 -drive format=raw,file="$BIN_FILE" -nographic -serial mon:stdio
+qemu-system-x86_64 -drive format=raw,file="$BIN_FILE" #-nographic -serial mon:stdio
