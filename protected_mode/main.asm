ec2fc6e26a3d5b8602a5d51727fee2eceed27ac0
Caleb Wolfe <cwolfe651@gmai.com>
2026-02-21 23:43:00 -0800

    save work for protected_mode start

--- protected_mode/main.asm
+++ protected_mode/main.asm
@@ -9,14 +9,33 @@ db 'Hello, World!', 0 ; <-- The zero on the end tells our routine
 GOODBYE_MSG:
 db 'Goodbye!', 0
 
-mov ebx, HELLO_MSG ; Use BX as a parameter to our function, so
-call print_string_pm
+mov bx, HELLO_MSG ; Use BX as a parameter to our function, so
+call print_string
 
-mov ebx, GOODBYE_MSG
-call print_string_pm
+mov bx, MSG_REAL_MODE
+call print_string
+
+mov bx, GOODBYE_MSG
+call print_string
+
+call switch_to_pm 
 
 jmp $ ; Hang
 ; Padding and magic number.
-%include "vga_example.asm"
+%include "print_string.asm"
+%include "gdt.asm"
+%include "print_string_pm.asm"
+%include "switch_to_pm.asm"
+
+; This is where we arrive after switching to and 
+; initializing protected mode
+[bits 32]
+BEGIN_PM:
+  move ebx, MSG_PROC_MODE
+  call print_string_pm
+  jmp $ ; hang 
+
+MSG_REAL_MODE db "Started in 16-bit real mode"
+MSG_PROC_MODE db "landed in 32-bit protected mode"
 times 510-($-$$) db 0
 dw 0xaa55
---
+++ protected_mode/switch_to_pm.asm
@@ -0,0 +1,27 @@
+lgdt [gdt_descriptor] ; load global descriptor table
+
+; switch to protected mode
+mov eax, cr0 
+or eax, 0x1 
+move cr0, eax
+
+jmp CODE_SEG:init_pm ;Make far jump to new segment to 
+                     ; 32-bit code. Forces CPU to  
+                     ; Flush pipeline of pre-fetched and real mode decoded 
+                     ; instructions. Flushing prvents unwanted problems
+[bits 32]
+; Initialise registers and the stack once in PM
+init_pm:
+ ; now in PM, load new segments 
+ mov ax, DATA_SEG
+ mov ds, ax
+ mov ss, ax
+ mov es, ax
+ mov fs, ax
+ mov gs, ax
+
+ mov ebp, 0x9000 ; Update our stack position so it is right
+ mov esp, ebp    ; at the top of free space
+
+call BEGIN_PM
+
