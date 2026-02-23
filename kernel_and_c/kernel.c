a6d401f830e43b485959fa462c9392862479583a
Caleb Wolfe <cwolfe651@gmai.com>
2026-02-22 22:01:09 -0800

    kernel_and_c: add simple kernel and boot asm here we go, yolo baby

---
+++ kernel_and_c/kernel.c
@@ -0,0 +1,7 @@
+void main() {
+  // Create a pointer char 
+  // point it at the first cell in memory
+  char* video_memory = (char*) 0xb8000;
+  // At the address of video_memory, store 'X'
+  *video_memory = 'X' ;
+}
