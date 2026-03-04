bee34c15bcea778093dae294c4898a9f7b6bd4c6
Caleb Wolfe <cwolfe651@gmai.com>
2026-03-03 22:16:04 -0800

    remove commented X expirment code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,10 +2,7 @@
 #include "../drivers/screen.h"
 
 void main() {
-  // Create a pointer char 
-  // point it at the first cell in memory
-  char* video_memory = (char*) 0xb8000;
-  // At the address of video_memory, store 'X'
-  // *video_memory = 'X' ;
-  print('hello world');
+  char* message;
+  message = "Hello World!";
+  print(message);
 }
